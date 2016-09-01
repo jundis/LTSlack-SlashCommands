608521c40daebd1a4c236021cb79b353c7bdfb87
jundis <jundis@gmail.com>
2016-09-01 16:30:04 -0500

    Initial slack connection. Not complete.

--- lt-config.php
+++ lt-config.php
@@ -6,4 +6,8 @@
 $authorization = "LT API KEY HERE"; //Enter your LabTech REST API key here.
 $labtech = "https://lt.domain.tld" //Enter your LabTech FQDN here.
 
+
+//Change optional
+$helpurl = "https://github.com/jundis/LTSlack-SlashCommands"; //Set your help article URL here.
+
 ?>
--- ltslack.php
+++ ltslack.php
@@ -3,7 +3,24 @@ ini_set('display_errors', 1); //Display errors in case something occurs
 header('Content-Type: application/json'); //Set the header to return JSON, required by Slack
 require_once 'lt-config.php';
 
-$urlticketdata = $labtech . "/WCC2/api/ComputerStubs?$filter=ComputerID eq " . $_GET['id']; //Set ticket API url
+if(empty($_GET['token']) || ($_GET['token'] != $slacktoken)) die; //If Slack token is not correct, kill the connection. This allows only Slack to access the page for security purposes.
+if(empty($_GET['text'])) die; //If there is no text added, kill the connection.
+
+$exploded = explode(" ",$_GET['text']); //Explode the string attached to the slash command for use in variables.
+
+if ($exploded[0]=="help") 
+{
+	$test=json_encode(array("parse" => "full", "response_type" => "in_channel","text" => "Please visit " . $helpurl . " for more help information","mrkdwn"=>true));
+	echo $test;
+	return;
+}
+$computerurl = NULL; //Set to null just in case.
+
+if(is_numeric($exploded[0])) { //If it detects an ID, set the URL to use ID.
+	$computerurl = $labtech . "/WCC2/api/Computers?$filter=ComputerID eq " . $exploded[0]; //Set ticket API url
+} else {
+	$computerurl = $labtech . "/WCC2/api/Computers?$filter=Name eq '" . $exploded[0] . "'"; //Set the ticket API url to use name search.
+}
 
 $header_data =array(
  "Authorization: LTToken ". $authorization,
@@ -13,7 +30,7 @@ $ch = curl_init(); //Initiate a curl session_cache_expire
 
 //Create curl array to set the API url, headers, and necessary flags.
 $curlOpts = array(
-	CURLOPT_URL => $urlticketdata,
+	CURLOPT_URL => $computerurl,
 	CURLOPT_RETURNTRANSFER => true,
 	CURLOPT_HTTPHEADER => $header_data,
 	CURLOPT_FOLLOWLOCATION => true,
@@ -31,8 +48,28 @@ if (curl_error($ch)) {
 }
 curl_close($ch);
 
+//Funky conversion for LT Data.
 $dataTData = json_decode($curlBodyTData); //Decode the JSON returned by the CW API.
+$dataTData = json_decode(json_encode($dataTData->value),true);
+$dataTData = $dataTData[0];
 
-echo $curlBodyTData;
+$return =array(
+	"parse" => "full",
+	"response_type" => "in_channel",
+	"attachments"=>array(array(
+		"fallback" => "Info on System " . $dataTData["Name"] . " (" . $dataTData["ComputerID"] . ")", //Fallback for notifications
+		"title" => "<" . $ticketurl . $dataTData -> id . "&companyName=" . $companyname . "|#" . $dataTData->id . ">: " . $dataTData->summary, //Return clickable link to ticket with ticket summary.
+		"pretext" => "Info on Ticket #" . $dataTData->id, //Return info string with ticket number.
+		"text" =>  $dataTData->company->identifier . " / " . $contact . //Return "Company / Contact" string
+		"\n" . $dateformat . " | " . $dataTData->status->name . //Return "Date Entered / Status" string
+		"\n" . $dataTData->resources, //Return assigned resources
+		"mrkdwn_in" => array(
+			"text",
+			"pretext"
+			)
+		))
+	);
+
+echo json_encode($return, JSON_PRETTY_PRINT); //Return properly encoded arrays in JSON for Slack parsing.
 
 ?>
